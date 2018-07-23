120b225c5a07b89d6651e37f203b9b227a47964c
Adarshreddy adelli <adarshreddy9849@gmail.com>
2018-07-23 21:24:12 +0530

    Update correct.py

--- correct.py
+++ correct.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 def words(text): return re.findall(r'\w+', text.lower())
 
-WORDS = Counter(words(open('big.txt').read()))
+WORDS = Counter(words(open('ash.txt').read()))
 
 def P(word, N=sum(WORDS.values())): 
     "Probability of `word`."
